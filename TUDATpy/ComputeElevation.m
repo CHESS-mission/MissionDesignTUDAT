bae72603b8c07889eb66032cf176ead84db00cb8
Nosudrum <arnaud.muller1308@gmail.com>
2020-11-26 14:39:17 +0100

    Old matlab files that are still required

---
+++ TUDATpy/ComputeElevation.m
@@ -0,0 +1,37 @@
+%% EPFL Spacecraft Team - CHESS Mission Design
+% Arnaud Muller & Antoine Clout
+% Autumn 2020
+%% Function description
+% This function computes and returns the elevation in degrees of a satellite as seen from a ground
+% station using ECI coordinates of both.
+
+% The inputs are n*3 double arrays of identical dimensions.
+%% Test values
+% PosGS=[0 0 1;0 0 1;0 0 1;0 0 1];
+% PosSat=[0 0 2;1 1 1;1 1 2;-1 -1 -2];
+
+function [elev, Position_CHESS_1] = ComputeElevation(GS_position,CHESS_State)
+
+Epochs_CHESS_10 = CHESS_State(:,1); % Array with every 10s epochs
+Epochs_CHESS_1 = (Epochs_CHESS_10(1):1:Epochs_CHESS_10(end))'; % Array with every 1s epochs
+
+x_CHESS_10 = CHESS_State(:,2); % Position every 10s
+y_CHESS_10 = CHESS_State(:,3); % Position every 10s
+z_CHESS_10 = CHESS_State(:,4); % Position every 10s
+
+
+x_CHESS_1 = interp1(Epochs_CHESS_10, x_CHESS_10, Epochs_CHESS_1); % Position every second
+y_CHESS_1 = interp1(Epochs_CHESS_10, y_CHESS_10, Epochs_CHESS_1); % Position every second
+z_CHESS_1 = interp1(Epochs_CHESS_10, z_CHESS_10, Epochs_CHESS_1); % Position every second
+
+
+PosGS = GS_position;
+Position_CHESS_1 = [x_CHESS_1,y_CHESS_1,z_CHESS_1];
+
+GS=Position_CHESS_1-PosGS;
+elev=asind((dot(PosGS',GS')')./(sqrt(dot(PosGS',PosGS')').*sqrt(dot(GS',GS')')));
+
+end
+
+
+
